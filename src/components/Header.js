3ddb6a341daa3714a07a54170a05bcdb852f0be0
Gustavo Saiani <gustavo@poe.ma>
2016-12-26 17:33:12 -0200

    Add components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default ({title}) => (
+  <nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="#">{title}</a>
+      </div>
+    </div>
+  </nav>
+);
